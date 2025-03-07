b912a9b239fe22ad921740d962e36f0fec6043c9
jnahan <jenna5376@gmail.com>
2025-03-07 14:50:09 -0500

    started implementing social share

--- src/pages/Download/index.jsx
+++ src/pages/Download/index.jsx
@@ -3,19 +3,30 @@ import { useFrame } from "../../hooks/FrameContext";
 import BackHeader from "../../components/BackHeader/BackHeader";
 import "./index.css";
 import { toPng } from "html-to-image";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import useRefreshWarning from "../../hooks/useRefreshWarning";
+import Options from "../../components/Options/Options";
+
+import TikTok from "../../assets/logos/TikTok.svg";
+import Instagram from "../../assets/logos/Instagram.svg";
+import print from "../../assets/logos/print.svg";
 
 function Download() {
   useRefreshWarning();
 
   const frame = useFrame();
   const frameRef = useRef(null);
+  const [frameImage, setFrameimage] = useState("");
 
-  console.log(frame);
+  // update print and share icon designs
+  const options = [
+    { name: "Instagram", image: Instagram },
+    { name: "TikTok", image: TikTok },
+    { name: "print", image: print },
+  ];
 
-  const downloadImage = () => {
+  useEffect(() => {
     toPng(frameRef.current, {
       cacheBust: false,
       width: frameRef.current.offsetWidth * 4,
@@ -24,74 +35,74 @@ function Download() {
         transform: "scale(4)",
         transformOrigin: "top left",
       },
-    })
-      .then((dataUrl) => {
-        const link = document.createElement("a");
-        link.download = "nerofilm.png";
-        link.href = dataUrl;
-        link.click();
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    }).then((dataUrl) => {
+      setFrameimage(dataUrl);
+    });
+  }, []);
+
+  const downloadImage = () => {
+    const link = document.createElement("a");
+    link.download = "nerofilm.png";
+    link.href = frameImage;
+    link.click();
   };
 
   const printImage = () => {
-    toPng(frameRef.current, {
-      cacheBust: false,
-      width: frameRef.current.offsetWidth * 4,
-      height: frameRef.current.offsetHeight * 4,
-      style: {
-        transform: "scale(4)",
-        transformOrigin: "top left",
-      },
-    })
-      .then((dataUrl) => {
-        // hidden iframe to inject print content
-        const iframe = document.createElement("iframe");
-        iframe.style.position = "absolute";
-        iframe.style.width = "0";
-        iframe.style.height = "0";
-        iframe.style.border = "none";
-        document.body.appendChild(iframe);
+    const iframe = document.createElement("iframe");
+    iframe.style.position = "absolute";
+    iframe.style.width = "0";
+    iframe.style.height = "0";
+    iframe.style.border = "none";
+    document.body.appendChild(iframe);
 
-        const doc = iframe.contentWindow.document;
-        doc.open();
-        doc.write(`
-        <html>
-          <head>
-            <title>Print Image</title>
-            <style>
-              @page {
-                size: letter;
-                margin: 0;
-              }
-              body {
-                margin: 0;
-                padding: 0;
-              }
-              img {
-                width: 2in;
-                height: 6in;
-                position: absolute;
-                top: 0;
-                left: 0;
-              }
-            </style>
-          </head>
-          <body>
-            <img src="${dataUrl}" alt="Printable" />
-          </body>
-        </html>
-      `);
-        doc.close();
+    const doc = iframe.contentWindow.document;
+    doc.open();
+    doc.write(`
+    <html>
+      <head>
+        <title>Print Image</title>
+        <style>
+          @page {
+            size: letter;
+            margin: 0;
+          }
+          body {
+            margin: 0;
+            padding: 0;
+          }
+          img {
+            width: 2in;
+            height: 6in;
+            position: absolute;
+            top: 0;
+            left: 0;
+          }
+        </style>
+      </head>
+      <body>
+        <img src="${frameImage}" alt="Printable" />
+      </body>
+    </html>
+  `);
+    doc.close();
 
-        iframe.contentWindow.focus();
-        iframe.contentWindow.print();
-      })
-      .catch((err) => {
-        console.error("Error generating image:", err);
-      });
+    iframe.contentWindow.focus();
+    iframe.contentWindow.print();
+  };
+
+  // todo : handle ig / tiktok share
+  const handleShare = (option) => {
+    switch (option) {
+      // sharing should upload film to story
+      case "Instagram":
+        break;
+      // sharing should upload timelapse
+      case "TikTok":
+        break;
+      case "print":
+        printImage();
+        break;
+    }
   };
 
   return (
@@ -109,18 +120,19 @@ function Download() {
               stickers={frame.stickers}
             />
           </div>
+          <Options
+            options={options}
+            onClick={(option) => handleShare(option)}
+            selected={frame.filter}
+          />
         </section>
         <section className="btns">
           <button className="btn" onClick={() => downloadImage()}>
             Download image
           </button>
-          <Link className="btn" role="button" to={"/"}>
+          <Link className="btn btn-secondary" role="button" to={"/"}>
             Return home
           </Link>
-          <button className="btn" onClick={() => printImage()}>
-            Print Image
-          </button>
-          {/* <button>Print image</button> */}
         </section>
       </section>
     </div>
