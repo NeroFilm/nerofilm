404b5c6d94709d0dfdbf6a2c2aa804ac54acd95f
ivapark <cmpark1656@gmail.com>
2025-03-07 03:05:32 -0500

    added privacy policy

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,21 @@
-import "./footer.css";
+import { Link } from "react-router-dom";
+import "./Footer.css"; // Import the CSS file
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <h1 className="logo logo-small">NeroFilm</h1>
-      <a href="mailto:contact.nerofilm@gmail.com">contact.nerofilm@gmail.com</a>
+      {/* Brand Name */}
+      <div className="logo logo-small">NeroFilm</div>
+
+      {/* Right Side Content (Email & Privacy Policy) */}
+      <div className="footer-right">
+        <a href="mailto:contact.nerofilm@gmail.com" className="footer-email">
+          contact.nerofilm@gmail.com
+        </a>
+        <Link to="/privacy-policy" className="privacy-policy-link">
+          Privacy Policy
+        </Link>
+      </div>
     </footer>
   );
 };
